016cccf39f65f2bbfc5cc6a3fa2b633e09210309
Lucas-Masaba <masabaluke@gmail.com>
2022-05-05 19:45:40 +0300

    add update method to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,4 +13,23 @@ class UsersController < ApplicationController
     render json: @user, status: :ok
   end
 
+  # POST /users
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: { errors: @user.errors.full_messages }, 
+            status: :unprocessible_entity
+    end
+  end
+
+  # PUT /users/{username}
+  def update
+    unless @user.update(user_params)
+      render json: { errors: @user.errors.full_messages },
+              status: :unprocessible_entity
+    end
+  end
+
 end
