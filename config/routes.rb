0d85e4ebbd9fab77ddb85dccb502ec897797b656
keroloussamy <keroloussamy98@gmail.com>
2022-05-05 19:32:37 +0200

    Add login routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resource :users
+  post '/auth/login', to: 'authentication#login'
   # Defines the root path route ("/")
   # root "articles#index"
 end
