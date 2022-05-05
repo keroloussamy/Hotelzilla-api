87e42472142d2f212ae0eff7ff95fd0c195e0542
keroloussamy <keroloussamy98@gmail.com>
2022-05-05 21:42:12 +0200

    Add one-many relation between hotel and rooms

--- db/migrate/20220505193530_create_rooms.rb
+++ db/migrate/20220505193530_create_rooms.rb
@@ -7,6 +7,8 @@ class CreateRooms < ActiveRecord::Migration[7.0]
       t.integer :capacity
       t.string :image
 
+      t.references :hotel, null: false, foreign_key: true
+
       t.timestamps
     end
   end
