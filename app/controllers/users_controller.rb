feb1fb57d487e513bb8c7240cdece37cd81cf145
Lucas-Masaba <masabaluke@gmail.com>
2022-05-05 19:38:49 +0300

    add index method to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+  skip_before_action :authenticate_request, only: [:create]
+  before_action :set_user, only: [:show, :destroy]
+
+  # GET /users
+  def index 
+    @users = User.all
+    render json: @users, status: :ok
+  end
 end
