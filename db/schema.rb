b7ed5987029cc366080bd23828835db382a0e085
keroloussamy <keroloussamy98@gmail.com>
2022-05-06 15:16:07 +0200

    run `rails db:migrate`

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_05_194608) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_06_131300) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -60,7 +60,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_05_194608) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "role"
+    t.string "role", default: "User"
   end
 
   add_foreign_key "hotels", "cities"
