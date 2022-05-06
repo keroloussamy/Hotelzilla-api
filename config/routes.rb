d02204fbf04d72ebc11dc27b1bc36ead244570d7
keroloussamy <keroloussamy98@gmail.com>
2022-05-07 00:20:43 +0200

    Fix tow typo errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resource :users
+  resources :users
   post '/auth/login', to: 'authentication#login'
   # Defines the root path route ("/")
   # root "articles#index"
