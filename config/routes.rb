c15d57695cdd5c6217cb2dd941ba8a70b1f597d1
Lucas-Masaba <masabaluke@gmail.com>
2022-05-06 20:24:21 +0300

    add hotels resources to destroy

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,16 @@ Rails.application.routes.draw do
   post '/auth/login', to: 'authentication#login'
   # Defines the root path route ("/")
   # root "articles#index"
+
+  namespace :api, defaults: { format: :json} do
+    resources :hotels, only: [:destroy]
+
+
+  
+    # post 'users/register', to: 'authentication#register'
+    # post 'users/login', to: 'authentication#login'
+    # get 'posts', to: 'api#posts_index'
+    # get 'comments', to: 'api#list_comments'
+    # post 'new_comment', to: 'api#create_comment'
+  end
 end
