178f0e6320ef86e7ebda025dd13aa0739476891a
Lucas-Masaba <masabaluke@gmail.com>
2022-05-06 18:11:51 +0300

    many to many relationships

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,6 @@
 class Room < ApplicationRecord
+  has_many :reservations
+  has_many :users, through: :reservations
+
   belongs_to :hotel
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+
+  has_many :reservations
+  has_many :rooms, through: :reservations
+
   require 'securerandom'
 
   has_secure_password
