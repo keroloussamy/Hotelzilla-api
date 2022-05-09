c827eaf2642ae383b11dcdff07a76c6939069b16
Lucas-Masaba <masabaluke@gmail.com>
2022-05-09 16:32:58 +0300

    fix typo in application.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
   include JsonWebToken
+  include Response
 
   before_action :authenticate_request
 
@@ -7,7 +8,7 @@ class ApplicationController < ActionController::API
 
   def authenticate_request
     header = request.headers['Authorization']
-    header = header.split.last if headers
+    header = header.split(' ').last if header
     decoded = jwt_decode(header)
     @current_user = User.find(decoded[:user_id])
   end
