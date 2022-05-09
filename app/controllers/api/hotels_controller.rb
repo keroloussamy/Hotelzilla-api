911790e2c6ec5e06a69f2ad7da7172368aeae75d
Lucas-Masaba <masabaluke@gmail.com>
2022-05-09 16:34:29 +0300

    Add hotels controller

--- app/controllers/api/hotels_controller.rb
+++ app/controllers/api/hotels_controller.rb
@@ -1,6 +1,39 @@
 module Api
   class HotelsController < ApplicationController
-    before_action :set_hotel
+    # before_action :set_hotel
+
+    def index
+      @hotels = Hotel.all
+  
+      render json: @hotels
+    end
+  
+    # GET /hotels/1
+    def show
+      @hotel = Hotel.find(params[:id])
+      render json: @hotel
+    end
+  
+    # POST /hotels
+    def create
+      @hotel = Hotel.new(hotel_params)
+      # @hotel.city_id = City.id
+
+      if @hotel.save
+        render json: @hotel, status: :created, location: @hotel
+      else
+        render json: @hotel.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # PATCH/PUT /hotels/1
+    def update
+      if @hotel.update(hotel_params)
+        render json: @hotel
+      else
+        render json: @hotel.errors, status: :unprocessable_entity
+      end
+    end
     
     def destroy
       @hotel.destroy
@@ -9,8 +42,12 @@ module Api
 
     private
 
-    def set_hotel
-      @hotel = Hotel.find(params[:id]) 
+    # def set_hotel
+    #   @hotel = Hotel.find(params[:id]) 
+    # end
+
+    def hotel_params
+      params.permit(:name, :city_id)
     end
 
   end
