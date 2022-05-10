a13626fcbc9d760f8e65a32713dc8b0a8396424a
keroloussamy <keroloussamy98@gmail.com>
2022-05-10 23:15:11 +0200

    Update cities controller

--- app/controllers/api/cities_controller.rb
+++ app/controllers/api/cities_controller.rb
@@ -1,55 +1,8 @@
 module Api
   class CitiesController < ApplicationController
-    # before_action :set_city
-
     def index
       @cities = City.all
-
       render json: @cities
-    end
-
-    # GET /cities/1
-    def show
-      @city = City.find(params[:id])
-      render json: @city
-    end
-
-    # POST /cities
-    def create
-      @city = City.new(city_params)
-
-      if @city.save
-        render json: @city, status: :created, location: api_city_path(@city)
-      else
-        render json: @city.errors, status: :unprocessable_entity
-      end
-    end
-
-    # PATCH/PUT /cities/1
-    def update
-      @city = City.find(params[:id])
-
-      if @city.update(city_params)
-        render json: @city
-      else
-        render json: @city.errors, status: :unprocessable_entity
-      end
-    end
-
-    def destroy
-      @city = City.find(params[:id])
-      @city.destroy
-      render json: { city: @city, message: 'City has successfully been deleted' }
-    end
-
-    private
-
-    # def set_city
-    #   @city = City.find(params[:id])
-    # end
-
-    def city_params
-      params.permit(:name)
     end
   end
 end
