a98ec5ad174c5d8fcf910c2078b6b5acba0e2a08
keroloussamy <keroloussamy98@gmail.com>
2022-05-11 00:19:39 +0200

    Add 'has_one_attached :image' to model

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -4,4 +4,7 @@ class Hotel < ApplicationRecord
   has_many :reservations
   has_many :users, through: :reservations
   has_many :room_types, through: :reservations
+
+  has_one_attached :image
+  
 end
