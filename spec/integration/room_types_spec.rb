286d775108bbf2eb3347b01a948bd10209408701
Lucas-Masaba <masabaluke@gmail.com>
2022-05-13 00:02:32 +0300

    Add rooms api documentation

---
+++ spec/integration/room_types_spec.rb
@@ -0,0 +1,27 @@
+require 'swagger_helper'
+
+RSpec.describe 'RoomTypes API' do
+  path '/api/room_types' do
+    get 'Retrieves types of rooms' do
+      tags 'Room Types'
+      produces 'application/json', 'application/xml'
+
+      response '200', 'Room Types found' do
+        schema type: :object,
+               properties: {
+                 name: { type: :string },
+                 description: { type: :string },
+                 price: { type: :integer }
+
+               },
+               required: %w[name description price]
+
+        run_test!
+      end
+
+      response '404', 'Room Types not found' do
+        run_test!
+      end
+    end
+  end
+end
