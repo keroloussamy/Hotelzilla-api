de2c769db8f6efd8c2523b04aa98c7468e4500ea
Lucas-Masaba <masabaluke@gmail.com>
2022-05-13 00:12:53 +0300

    Add reservations post documenation

--- spec/integration/reservations_spec.rb
+++ spec/integration/reservations_spec.rb
@@ -24,4 +24,34 @@ RSpec.describe 'Reservations API' do
       end
     end
   end
+
+  path '/api/reservations' do
+
+    post 'Creates a reservation' do
+      tags 'Reservations'
+      consumes 'application/json', 'application/xml'
+      parameter name: :reservation, in: :body, schema: {
+        type: :object,
+        properties: {
+          date: { type: :string },
+          room_type_id: { type: :integer },
+          hotel_id: { type: :integer }
+
+        },
+        required: [ 'name', 'room_type_id', 'hotel_id' ]
+      }
+
+      response '201', 'Reservation created' do
+        let(:reservation) {   { name:'Single', description: 'A room assigned to one person. May have one or more beds.', price: 300} }
+        run_test!
+      end
+
+      response '422', 'invalid request' do
+        let(:reservation) { 'foo' } 
+        run_test!
+      end
+    end
+  end
+
+
 end
