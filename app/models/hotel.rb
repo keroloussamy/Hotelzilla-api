c84d4870cfe51f970ca39404fe7f79fbd33c9d50
keroloussamy <keroloussamy98@gmail.com>
2022-05-12 17:39:28 +0200

    Make when delete hotel delete the reservation too

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,7 +1,7 @@
 class Hotel < ApplicationRecord
   belongs_to :city
 
-  has_many :reservations
+  has_many :reservations, dependent: :destroy
   has_many :users, through: :reservations
   has_many :room_types, through: :reservations
 
